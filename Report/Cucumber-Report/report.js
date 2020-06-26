b0a4055e45ecd4f72b0ce0f1d4ec5b24c2176037
Khurram <mohdkhurum@gmail.com>
2020-06-25 21:55:24 -0400

    updated

--- Report/Cucumber-Report/report.js
+++ Report/Cucumber-Report/report.js
@@ -47,21 +47,21 @@ formatter.match({
   "location": "LoginTest.I_Open_Chrome_Browser()"
 });
 formatter.result({
-  "duration": 7466099102,
+  "duration": 2904383303,
   "status": "passed"
 });
 formatter.match({
   "location": "LoginTest.I_go_to_the_Salesforce_application()"
 });
 formatter.result({
-  "duration": 1032339639,
+  "duration": 624114419,
   "status": "passed"
 });
 formatter.match({
   "location": "LoginTest.I_maximize_my_window()"
 });
 formatter.result({
-  "duration": 491817483,
+  "duration": 384625512,
   "status": "passed"
 });
 formatter.match({
@@ -74,7 +74,7 @@ formatter.match({
   "location": "LoginTest.I_enter_username(String)"
 });
 formatter.result({
-  "duration": 1317148481,
+  "duration": 1972019883,
   "status": "passed"
 });
 formatter.match({
@@ -87,21 +87,12 @@ formatter.match({
   "location": "LoginTest.I_enter_password(String)"
 });
 formatter.result({
-  "duration": 184618390,
+  "duration": 225324675,
   "status": "passed"
 });
-formatter.match({
-  "arguments": [
-    {
-      "val": "Login",
-      "offset": 13
-    }
-  ],
-  "location": "AccountPage.I_click_the_button(String)"
-});
+formatter.match({});
 formatter.result({
-  "duration": 975475915,
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.scenario({
   "line": 10,
@@ -157,79 +148,37 @@ formatter.step({
   "name": "I should see the Account record",
   "keyword": "Then "
 });
-formatter.match({
-  "arguments": [
-    {
-      "val": "Account_Tab",
-      "offset": 9
-    }
-  ],
-  "location": "AccountPage.I_click_tab(String)"
-});
+formatter.match({});
 formatter.result({
-  "duration": 2843611784,
-  "status": "passed"
+  "status": "undefined"
 });
-formatter.match({
-  "arguments": [
-    {
-      "val": "Accounts",
-      "offset": 9
-    }
-  ],
-  "location": "AccountPage.I_am_on_home_page(String)"
-});
+formatter.match({});
 formatter.result({
-  "duration": 433599670,
-  "status": "passed"
+  "status": "undefined"
 });
-formatter.match({
-  "arguments": [
-    {
-      "val": "new",
-      "offset": 13
-    }
-  ],
-  "location": "AccountPage.I_click_the_button(String)"
-});
+formatter.match({});
 formatter.result({
-  "duration": 852238382,
-  "status": "passed"
+  "status": "undefined"
 });
-formatter.match({
-  "location": "AccountPage.I_enter_Account_Name_field()"
-});
+formatter.match({});
 formatter.result({
-  "duration": 482802051,
-  "status": "passed"
+  "status": "undefined"
 });
-formatter.match({
-  "location": "AccountPage.I_should_see_the_Rating_field()"
-});
+formatter.match({});
 formatter.result({
-  "duration": 83390296,
-  "status": "passed"
+  "status": "undefined"
 });
-formatter.match({
-  "location": "AccountPage.I_select_Hot_from_the_Rating_field()"
-});
+formatter.match({});
 formatter.result({
-  "duration": 125304547,
-  "status": "passed"
+  "status": "undefined"
 });
-formatter.match({
-  "location": "AccountPage.I_click_the_save_button()"
-});
+formatter.match({});
 formatter.result({
-  "duration": 1652034210,
-  "status": "passed"
+  "status": "undefined"
 });
-formatter.match({
-  "location": "AccountPage.I_should_see_the_Account_record()"
-});
+formatter.match({});
 formatter.result({
-  "duration": 83831465,
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.background({
   "line": 2,
@@ -272,21 +221,21 @@ formatter.match({
   "location": "LoginTest.I_Open_Chrome_Browser()"
 });
 formatter.result({
-  "duration": 1676078621,
+  "duration": 1745445577,
   "status": "passed"
 });
 formatter.match({
   "location": "LoginTest.I_go_to_the_Salesforce_application()"
 });
 formatter.result({
-  "duration": 904584624,
+  "duration": 1813210293,
   "status": "passed"
 });
 formatter.match({
   "location": "LoginTest.I_maximize_my_window()"
 });
 formatter.result({
-  "duration": 405632849,
+  "duration": 428273731,
   "status": "passed"
 });
 formatter.match({
@@ -299,7 +248,7 @@ formatter.match({
   "location": "LoginTest.I_enter_username(String)"
 });
 formatter.result({
-  "duration": 1247622458,
+  "duration": 1803238970,
   "status": "passed"
 });
 formatter.match({
@@ -312,21 +261,12 @@ formatter.match({
   "location": "LoginTest.I_enter_password(String)"
 });
 formatter.result({
-  "duration": 355112170,
+  "duration": 424284590,
   "status": "passed"
 });
-formatter.match({
-  "arguments": [
-    {
-      "val": "Login",
-      "offset": 13
-    }
-  ],
-  "location": "AccountPage.I_click_the_button(String)"
-});
+formatter.match({});
 formatter.result({
-  "duration": 749955874,
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.scenario({
   "line": 22,
@@ -367,13 +307,9 @@ formatter.step({
   ],
   "keyword": "And "
 });
-formatter.match({
-  "location": "AccountPage.I_create_an_Account(DataTable)"
-});
+formatter.match({});
 formatter.result({
-  "duration": 2616175,
-  "error_message": "java.lang.Error: Unresolved compilation problem: \n\tThe method asMaps(Class\u003cK\u003e, Class\u003cV\u003e) in the type DataTable is not applicable for the arguments ()\n\n\tat stepDefination.AccountPage.I_create_an_Account(AccountPage.java:57)\n\tat ✽.And I create an Account(Feature/Account.feature:23)\n",
-  "status": "failed"
+  "status": "undefined"
 });
 formatter.uri("Feature/login.feature");
 formatter.feature({
@@ -505,22 +441,22 @@ formatter.match({
   "location": "LoginTest.I_Open_Chrome_Browser()"
 });
 formatter.result({
-  "duration": 2104163894,
+  "duration": 1824312401,
   "status": "passed"
 });
 formatter.match({
   "location": "LoginTest.I_go_to_the_Salesforce_application()"
 });
 formatter.result({
-  "duration": 1375313199,
-  "error_message": "org.openqa.selenium.WebDriverException: unknown error: cannot determine loading status\nfrom unknown error: cannot determine loading status\nfrom disconnected: received Inspector.detached event\n  (Session info: chrome\u003d83.0.4103.116)\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027Khurrams-MacBook-Air.local\u0027, ip: \u0027fe80:0:0:0:484:f53a:9a67:6865%en0\u0027, os.name: \u0027Mac OS X\u0027, os.arch: \u0027x86_64\u0027, os.version: \u002710.15.4\u0027, java.version: \u00271.8.0_241\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 83.0.4103.116, chrome: {chromedriverVersion: 83.0.4103.39 (ccbf011cb2d2b..., userDataDir: /var/folders/kf/456qvlr53l3...}, goog:chromeOptions: {debuggerAddress: localhost:49847}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: MAC, platformName: MAC, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:virtualAuthenticators: true}\nSession ID: 4084874128fe985aedcaa1fee6b72403\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\n\tat org.openqa.selenium.remote.RemoteWebDriver.get(RemoteWebDriver.java:277)\n\tat stepDefination.LoginTest.I_go_to_the_Salesforce_application(LoginTest.java:26)\n\tat ✽.When I go to the Salesforce application(Feature/login.feature:7)\n",
-  "status": "failed"
+  "duration": 832253283,
+  "status": "passed"
 });
 formatter.match({
   "location": "LoginTest.I_maximize_my_window()"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 445516265,
+  "status": "passed"
 });
 formatter.match({
   "arguments": [
@@ -532,7 +468,8 @@ formatter.match({
   "location": "LoginTest.I_enter_username(String)"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 1709430939,
+  "status": "passed"
 });
 formatter.match({
   "arguments": [
@@ -544,19 +481,12 @@ formatter.match({
   "location": "LoginTest.I_enter_password(String)"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 593445554,
+  "status": "passed"
 });
-formatter.match({
-  "arguments": [
-    {
-      "val": "Login",
-      "offset": 13
-    }
-  ],
-  "location": "AccountPage.I_click_the_button(String)"
-});
+formatter.match({});
 formatter.result({
-  "status": "skipped"
+  "status": "undefined"
 });
 formatter.scenario({
   "line": 24,
@@ -719,23 +649,22 @@ formatter.match({
   "location": "LoginTest.I_Open_Chrome_Browser()"
 });
 formatter.result({
-  "duration": 2101036699,
+  "duration": 1889638428,
   "status": "passed"
 });
 formatter.match({
   "location": "LoginTest.I_go_to_the_Salesforce_application()"
 });
 formatter.result({
-  "duration": 582797568,
+  "duration": 705537897,
   "status": "passed"
 });
 formatter.match({
   "location": "LoginTest.I_maximize_my_window()"
 });
 formatter.result({
-  "duration": 35598917,
-  "error_message": "org.openqa.selenium.NoSuchWindowException: no such window: window was already closed\n  (Session info: chrome\u003d83.0.4103.116)\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027Khurrams-MacBook-Air.local\u0027, ip: \u0027fe80:0:0:0:484:f53a:9a67:6865%en0\u0027, os.name: \u0027Mac OS X\u0027, os.arch: \u0027x86_64\u0027, os.version: \u002710.15.4\u0027, java.version: \u00271.8.0_241\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 83.0.4103.116, chrome: {chromedriverVersion: 83.0.4103.39 (ccbf011cb2d2b..., userDataDir: /var/folders/kf/456qvlr53l3...}, goog:chromeOptions: {debuggerAddress: localhost:49867}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: MAC, platformName: MAC, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:virtualAuthenticators: true}\nSession ID: 42c2dcb1cc3231b0d8a3546cf71bceb4\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:609)\n\tat org.openqa.selenium.remote.RemoteWebDriver$RemoteWebDriverOptions$RemoteWindow.maximize(RemoteWebDriver.java:837)\n\tat stepDefination.LoginTest.I_maximize_my_window(LoginTest.java:69)\n\tat ✽.And I maximize my window(Feature/login.feature:8)\n",
-  "status": "failed"
+  "duration": 444009970,
+  "status": "passed"
 });
 formatter.match({
   "arguments": [
@@ -747,7 +676,8 @@ formatter.match({
   "location": "LoginTest.I_enter_username(String)"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 1693288796,
+  "status": "passed"
 });
 formatter.match({
   "arguments": [
@@ -759,19 +689,12 @@ formatter.match({
   "location": "LoginTest.I_enter_password(String)"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 505408277,
+  "status": "passed"
 });
-formatter.match({
-  "arguments": [
-    {
-      "val": "Login",
-      "offset": 13
-    }
-  ],
-  "location": "AccountPage.I_click_the_button(String)"
-});
+formatter.match({});
 formatter.result({
-  "status": "skipped"
+  "status": "undefined"
 });
 formatter.scenario({
   "line": 25,
